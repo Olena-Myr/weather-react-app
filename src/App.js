56702b5cb218da01dcd39d4b04269bf67fc38592
Olena-Myr <132211621+Olena-Myr@users.noreply.github.com>
2023-08-02 09:12:24 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <small>
-          This project was coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/olena-myronenko-ua/"
-            target="blank"
-          >
-            Olena Myronenko
-          </a>{" "}
-          ans is{" "}
-          <a
-            href="https://github.com/Olena-Myr/weather-react-app"
-            target="blank"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a href="https://lively-kataifi-5a4ec5.netlify.app/" target="blank">
-            hosted on Netlify
-          </a>
-          .
-        </small>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/olena-myronenko-ua/"
+              target="blank"
+            >
+              Olena Myronenko
+            </a>{" "}
+            ans is{" "}
+            <a
+              href="https://github.com/Olena-Myr/weather-react-app"
+              target="blank"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a href="https://lively-kataifi-5a4ec5.netlify.app/" target="blank">
+              hosted on Netlify
+            </a>
+            .
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
